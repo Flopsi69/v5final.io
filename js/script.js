9526dbf609f0fa53d29f534d53de5ea73b489e99
Ziller Dev <flopsi69@gmail.com>
2017-07-30 10:05:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+		navigation: true,
+		navigationPosition: 'right'
+	});
+});
